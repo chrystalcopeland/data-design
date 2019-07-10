9d4999de0d4b9e5168ff5d88db1bfb3aff368d2c
Chrystal Copeland <canderson73@cnm.edu>
2019-07-10 12:04:24 -0600

    Added more information for use case page

--- epic/use-case.php
+++ epic/use-case.php
@@ -1,7 +1,39 @@
+<h1>
+	Use Case
+</h1>
 <p>
 	Cara is planning 3 summer trips with her dog, Buddy. She wants to visit pet friendly national parks, eat at restauraunts
 	with patios so she can take her dog, and stay in pet friendly hotels.
 </p>
+<h2>
+	Preconditions
+</h2>
+	<ul>
+		<li>
+			User has an account and is logged in.
+		</li>
+		<li>
+			User is able to search pre-populated data for pet friendly destinations, restaurants and hotels
+		</li>
+	</ul>
+<h2>
+	Postconditions
+</h2>
+	<ul>User can track destinations to plan a trip with her pet</ul>
+<h1>
+	Interaction Flow
+</h1>
+<h2>
+	Steps Cara must take to plan a trip
+</h2>
+<ol>
+	<li>
+		navigates to website and enters a city destination
+	</li>
+	<li>
+		search returns a list of petfriendly places and things to do at the destination.
+	</li>
+</ol>
 <div>
 		<a href="index.php">Home</a></li>
 </div>
